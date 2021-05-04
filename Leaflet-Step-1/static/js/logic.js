f94ffdb524e36b57782ff7fe43f61d3920153748
kerryharp <kharp@umn.edu>
2021-05-04 00:05:25 -0500

    got map working, step 1

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -2,81 +2,72 @@ console.log("logic file");
 
 // Folow syntax of Mapping I, lesson 10
 
-// Variable for API endpoint
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
-
-// Perform a GET request to the query URL
-d3.json(queryUrl, function (data) {
-    // Once we get a response, send the data.features object to the createFeatures function
-    createFeatures(data.features);
-});
-
-
-function createFeatures(earthquakeData) {
-
-    // Define a function we want to run once for each feature in the features array
-    // Give each feature a popup describing the place and time of the earthquake
-    function onEachFeature(feature, layer) {
-        layer.bindPopup("<h3>" + feature.properties.place +
-            "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-    }
-
-    // Create a GeoJSON layer containing the features array on the earthquakeData object
-    // Run the onEachFeature function once for each piece of data in the array
-    var earthquakes = L.geoJSON(earthquakeData, {
-        onEachFeature: onEachFeature
-    });
-
-    // Sending our earthquakes layer to the createMap function
-    createMap(earthquakes);
-}
-
-
-// Creating map object
+// Create map object
 var myMap = L.map("map", {
-    center: [40.7128, -94.0059],
+    center: [33.5, -99.5],
     zoom: 4
 });
 
-// Adding tile layer
-var greyMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 8,
-    zoomOffset: -1,
-    id: "mapbox/light-v10",
+// Adding light mode tile layer to the map
+L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "light-v10",
     accessToken: API_KEY
-});
+}).addTo(myMap);
 
-greyMap.addTo(myMap);
+// Store API endpoint 
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 
-// Perform GET request to the query url
-d3.json(queryUrl, function (data) {
+// Grab the data with d3
+d3.json(queryUrl).then(function (data) {
+
+    // Create function to color cicles according to earthquake magnitudes
+    function getColor(colors) {
+        return colors >= 5 ? "red" :
+        colors >= 4 ? "orange" :
+        colors >= 3 ? "yellow" :
+        colors >= 2 ? "green" :
+        colors >= 1 ? "blue" : "purple";
+    }
 
     // Grab the features data
     var features = data.features;
 
     for (var i = 0; i < features.length; i++) {
 
-        //Define magnitudes and coordinates of the earthquakes
+        //Define variable magnitudes and coordinates of the earthquakes
         var magnitudes = features[i].properties.mag;
         var coordinates = features[i].geometry.coordinates;
 
-        // Creating a GeoJSON layer with the retrieved data
-        L.geoJson(data).addTo(myMap);
-    }
+        // Add circles to map
+        L.circle(
+            [coordinates[1], coordinates[0]], {
+            fillOpacity: 0.5,
+            fillColor: getColor(magnitudes),
+            color: "black",
+            weight: 0.5,
+            radius: magnitudes * 15000
+        }).bindPopup("<h3>" + features[i].properties.place +
+            "</h3><hr><p>" + new Date(features[i].properties.time) +
+            '<br>' + '[' + coordinates[1] + ', ' + coordinates[0] + ']' + "</p>").addTo(myMap);
+    };
+
+    // // Legend for the chart
+    // var legend = L.control({ position: 'bottomright' });
+    // legend.onAdd = function () {
+
+    //     var div = L.DomUtil.create('div', 'info legend'),
+    //         grades = [0, 1, 2, 3, 4, 5],
+    //         labels = [];
+
+    //     // loop through our magnitude intervals and generate a label with a colored square for each interval
+    //     for (var i = 0; i < grades.length; i++) {
+    //         div.innerHTML +=
+    //             '<i style="background:' + getColor(grades[i]) + '"></i> ' +
+    //             grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    //     }
+    //     return div;
+    // };
+    // legend.addTo(myMap);
 });
-
-
-//Retrieve the features
-// var features = data.features;
-// console.log(features);
-
-
-
-// Adding legend
-// var legend = L.control({position: "bottomright"})
-
-// look at mapping class 1, exercise 8 & 9 for looping and pushing markers.
-
-// look at mapping class 1, exercise 10 for geoexample
